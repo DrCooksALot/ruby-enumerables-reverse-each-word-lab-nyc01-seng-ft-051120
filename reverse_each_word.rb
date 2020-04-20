865ed31b3649adac8a138f5f0d7b5a11c8d50227
Eli Nass <eliftw@gmail.com>
2020-04-20 07:07:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,3 @@
 def reverse_each_word(sentence)
-  reveresed_sentence = []
-  sentence_array = sentence.split(" ")
-  sentence_array.each { |word|
-    reveresed_word = []
-    word_array = word.split("")
-    word_array.each { |letter|
-      reveresed_word.unshift(letter)
-    }
-    reveresed_sentence.push(reveresed_word)
-  }
-  return_sentence = reveresed_sentence.map { |word|
-    word.join
-    
-  }
-  return_sentence.join(" ")
+  sentence_array = sentence.split
 end
